9fd5c4f6a676180e00b10274541527645d7139c1
Niki9526 <n01591561@humber.ca>
2026-01-13 20:56:08 -0500

    added endpoints for CRUD operations

---
+++ labs/dev/lab1/server.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+//let personData =  require('./data.json');
+let personData = require('./modules/peopleDataCRUD.js');
+
+app.use(express.json()); // body parsing middleware to handle JSON in the body
+
+app.get("/",(req,res)=>{
+    console.log(personData[0].firstName)
+    res.send(`Hello`)
+})
+
+//Endpoints
+//CREATE
+//POST
+//add a new person 
+app.post("/api/items",async(req,res)=>{
+    res.status(201).send({ message: await personData.addAPerson(
+        req.body.firstName,
+        req.body.lastName,
+        req.body.email
+    )})
+});
+
+//READ
+//GET
+// get all people
+app.get("/api/items", async(req,res)=>{
+    res.status(200).send(await personData.getAllPeople());
+});
+
+//GET
+//get person from people list data
+app.get("/api/items/:itemId",async (req,res)=>{
+    try{
+        res.status(200).send(await personData.getPeopleById(req.params.itemId));
+
+    } catch (err){
+        res.status(404).send({message: err})
+    }
+})
+
+
+//UPDATE (PUT)
+//update a person by there ID
+app.put("/api/items/:itemId",async(req,res)=>{
+    // if (req.body.firstName !== undefined) {
+    //     newFirstName = req.body.firstName;
+    // }
+    //  if (req.body.lastName !== undefined) {
+    //     newLastName = req.body.lastName;
+    // }
+    //  if (req.body.email !== undefined) {
+    //     newEmail = req.body.email;
+    // }
+
+    try{
+        res.status(200).send( await personData.updatePersonById(
+            req.params.itemId,
+            req.body.firstName,
+            req.body.lastName,
+            req.body.email
+         ))
+
+    } catch (err){
+        res.status(404).send({message: err})
+
+    }
+})
+
+//DELETE
+//delete a person by ID
+app.delete("/api/items/:itemId",async(req,res)=>{
+    try{
+        res.status(200).send({message: await personData.deletePersonById(req.params.itemId)} );
+
+    } catch (err) {
+        res.status(404).send({message: err})
+    }
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Press control + c to exit`);
+});
