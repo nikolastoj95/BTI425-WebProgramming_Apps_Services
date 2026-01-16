20fc9bf89b618e90082a4a85b7d0705f54d46331
Niki9526 <n01591561@humber.ca>
2026-01-16 18:05:22 -0500

    added two get routes show html

--- class_weeks/class_week02/week02-bootstrap/server.js
+++ class_weeks/class_week02/week02-bootstrap/server.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
+const path = require ("path");
 
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname + "/views/home.html")
+})
 
+app.get("/bootstrap",(req,res)=>{
+    res.sendFile(__dirname + "/views/bootstrap.html")
+});
 
 app.listen(port,()=>{
     console.log(`Server running on http://localhost:${port}/`)
