c8faf5acbde5544c0771e39c508801a069faaae5
Niki9526 <n01591561@humber.ca>
2026-01-29 23:25:59 -0500

    added all componets made to index

--- labs/dev/lab3/lab3-app/pages/index.js
+++ labs/dev/lab3/lab3-app/pages/index.js
@@ -1,11 +1,18 @@
 import ColorBox from "@/components/ColorBox";
+import CountDown from "@/components/CountDown";
+import Greeting from "@/components/Greeting";
+import RandomQuote from "@/components/RandomQuote";
+import Timer from "@/components/Timer";
 
 
 export default function Home() {
   return (
     <>
      <ColorBox color="orange"/>
-     
+     <Timer />
+     <Greeting name="Nikola" />
+     <RandomQuote />
+     <CountDown start={10} />
     </>
   );
 }
