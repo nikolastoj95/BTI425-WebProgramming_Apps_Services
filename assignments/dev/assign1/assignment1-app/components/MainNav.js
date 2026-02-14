1ae1689348eb9fe7245d5cbadeb0a14060baec5c
Niki9526 <n01591561@humber.ca>
2026-02-13 21:11:33 -0500

    added nav bar shown in whole app every page

--- assignments/dev/assign1/assignment1-app/components/MainNav.js
+++ assignments/dev/assign1/assignment1-app/components/MainNav.js
@@ -1,6 +1,28 @@
+import Link from "next/link";
+import { Container, Nav, Navbar } from "react-bootstrap";
+
 export default function MainNav () {
     return (
-        <>  
-        </>
-    )
+      <>
+        {/* <Container></Container>
+
+        <Nav></Nav>
+        <Navbar/> */}
+        {/* <Link></Link> */}
+        <Navbar className="fixed-top navbar-dark bg-dark"  >
+          <Container>
+            <Navbar.Brand >Nikola Stojanovic</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link as={Link} href="/">Books</Nav.Link>
+              <Nav.Link as={Link} href="/about">About</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <br />
+        <br />
+       
+
+        {/* <p>MainNav</p> */}
+      </>
+    );
 }
