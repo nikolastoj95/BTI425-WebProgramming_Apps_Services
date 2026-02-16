7892a9c1e1ce7bac455cd2d55be55e8a8a5bf97c
Niki9526 <n01591561@humber.ca>
2026-02-16 14:51:20 -0500

    added comments about this api route

--- class_weeks/class_week06-API_Routes/dev/app-week06/pages/api/hello.js
+++ class_weeks/class_week06-API_Routes/dev/app-week06/pages/api/hello.js
@@ -1,5 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const {method} = req;
+  
+switch (method) {
+  case "GET":
+    // Read data from your database
+    res.status(200).json({ name: "John Doe" });
+    break;
+ 
+  default:
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${method} Not Allowed`);
+    // 405 code is client error
 }
+
+
+
+
+
+
+
+
+  // res.status(200).json({ name: "John Doe" });
+}
+
+
+// // simlear to above same as in node  express
+// app.get("/api/hello", (req, res) => {
+//   res.status(200).json({ name: "John Doe" }); 
+// });
+
+// app.all("/api/hello", (req, res) => {
+//   res.status(200).json({ name: "John Doe" }); 
+// });
