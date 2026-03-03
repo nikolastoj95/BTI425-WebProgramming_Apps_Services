7da697e195fd7d52a9a27aa20faf3869b42639e2
Niki9526 <n01591561@humber.ca>
2026-03-03 14:33:02 -0500

    added the compoents

--- class_weeks/class_week07-Forms/dev-w07/w07app/pages/index.js
+++ class_weeks/class_week07-Forms/dev-w07/w07app/pages/index.js
@@ -1,8 +1,12 @@
+import SimpleForm from "@/components/SimpleForm";
 
 export default function Home() {
   return (
     <>
     <h1>Home</h1>
+    <hr />
+    <h2>Simple Form</h2>
+    <SimpleForm/>
     </>
   );
 }
