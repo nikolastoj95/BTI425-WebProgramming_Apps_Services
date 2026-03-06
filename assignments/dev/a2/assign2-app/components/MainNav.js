abab9412d4dbd06b33b28c9ce6af511474460532
Niki9526 <n01591561@humber.ca>
2026-03-05 20:56:40 -0500

    added link to home in nac brand, removed link that says books,

--- assignments/dev/a2/assign2-app/components/MainNav.js
+++ assignments/dev/a2/assign2-app/components/MainNav.js
@@ -6,9 +6,9 @@ export default function MainNav () {
       <>
         <Navbar className="fixed-top navbar-dark bg-dark"  >
           <Container>
-            <Navbar.Brand >Nikola Stojanovic</Navbar.Brand>
+            <Navbar.Brand as={Link} href="/" >Nikola Stojanovic</Navbar.Brand>
             <Nav className="me-auto">
-              <Nav.Link as={Link} href="/">Books</Nav.Link>
+              {/* <Nav.Link as={Link} href="/">Books</Nav.Link> */}
               <Nav.Link as={Link} href="/about">About</Nav.Link>
             </Nav>
           </Container>
