fe29b4061c71e48c7693a24bf744ea7808a580f3
Niki9526 <n01591561@humber.ca>
2026-03-05 20:57:12 -0500

    chansed page header card to take a subtext prop

--- assignments/dev/a2/assign2-app/components/PageHeader.js
+++ assignments/dev/a2/assign2-app/components/PageHeader.js
@@ -1,6 +1,6 @@
 import {Card } from "react-bootstrap";
 
-export default function PageHeader({text}){
+export default function PageHeader({text, subtext}){
     return (
       <>
         {/* <p>PageHeader</p> */}
@@ -8,7 +8,19 @@ export default function PageHeader({text}){
           <Card.Body>
             {text}
           </Card.Body>
+          
+          <Card.Body>
+           {subtext}
+          </Card.Body>
         </Card>
+        {/* <Card className="bg-light" style={{ width: "30rem" , textAlign: "center"}}>
+          <Card.Body>
+            <Card.Title>{text}</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              {subtext}
+            </Card.Subtitle>
+          </Card.Body>
+        </Card> */}
         <br />
       </>
     );
