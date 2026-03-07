ec13dcfd6f3963cc705531a165f1c95531ecfd5f
Niki9526 <n01591561@humber.ca>
2026-03-07 17:53:55 -0500

    added title for the fav page, added favourites atom reference, and showing workids saved

--- assignments/dev/a2/assign2-app/pages/favourites.js
+++ assignments/dev/a2/assign2-app/pages/favourites.js
@@ -1,7 +1,37 @@
+import PageHeader from "@/components/PageHeader";
+import { favouritesAtom } from "@/store";
+import { useAtom } from "jotai"
+import { Col, Container, Row } from "react-bootstrap";
+
+
 export default function Favourites(){
+    const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+    console.log(favouritesList)
+    // storing workIds
+
     return (
         <>
-            TODO Favourites
+            
+            {favouritesList.length > 0 ?(<PageHeader text={<h1>Favourites</h1>}  subtext={<p>All Your favourite books, in one place</p>}/> 
+            ):(
+            <PageHeader text={<h1>Nothing Here</h1>} subtext={<p>Try adding a book to the list</p>}/>)}
+
+            {favouritesList.length >0 && (
+                <Row className="gy-4">
+                    {favouritesList.map((workId)=> (
+                        <Col  lg={3} md={6} key={workId}>
+                            <p>{workId}</p> 
+                        </Col>
+
+                    ))}
+                    
+                </Row>
+
+            )}
+
+
+            
+
         </>
     )
 }
