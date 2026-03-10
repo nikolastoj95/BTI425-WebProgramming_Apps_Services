9f5d4a19eef28eb1d41b0b6268e36a27c4126767
Niki9526 <n01591561@humber.ca>
2026-03-10 19:36:56 -0400

    added table to get all users from atom of users

--- labs/dev/lab6/lab06-app/pages/index.js
+++ labs/dev/lab6/lab06-app/pages/index.js
@@ -1,8 +1,57 @@
+import { useAtom } from 'jotai';
+import usersData from '.././data/users.json';
+import { usersAtom } from '@/store';
+import { Table } from 'react-bootstrap';
+
 
 export default function Home() {
+  const [users, setUsers] = useAtom(usersAtom);
+  console.log(users)
+     
   return (
     <>
-     
+      <Table striped className="table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map(user => (
+            <tr key={user.id}>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+            </tr>
+          ))}
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+          </tr>
+        </tbody>
+      </Table>
+
+      {/* <table>
+          <thead>
+            <tr>
+              <th>A</th>
+              <th>B</th>
+              <th>C</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>1</td>
+              <td>1</td>
+            </tr>
+
+          </tbody>
+         
+         
+          
+        </table>
+       */}
     </>
   );
 }
