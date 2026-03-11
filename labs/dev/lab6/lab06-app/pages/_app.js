1c4a8d0c785469feea28c762c7facd9223445435
Niki9526 <n01591561@humber.ca>
2026-03-10 22:12:43 -0400

    added Layout to the whole app

--- labs/dev/lab6/lab06-app/pages/_app.js
+++ labs/dev/lab6/lab06-app/pages/_app.js
@@ -1,6 +1,7 @@
+import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Layout><Component {...pageProps} /></Layout> ;
 }
