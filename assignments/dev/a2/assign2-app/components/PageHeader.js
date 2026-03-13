1a1b66362882389c5969c21fa6a09be5a9652069
Niki9526 <n01591561@humber.ca>
2026-03-13 19:27:33 -0400

    added card display to show text and subtext

--- assignments/dev/a2/assign2-app/components/PageHeader.js
+++ assignments/dev/a2/assign2-app/components/PageHeader.js
@@ -4,15 +4,27 @@ export default function PageHeader({text, subtext}){
     return (
       <>
         {/* <p>PageHeader</p> */}
-        <Card className="bg-light">
+        {/* <div class="card border-light mb-3" style="max-width: 20rem;">
+          <div class="card-header">Header</div>
+          <div class="card-body">
+            <h4 class="card-title">Light card title</h4>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div> */}
+        <Card style={{border: "none", textAlign: "center"}} className="bg-light">
           <Card.Body>
-            {text}
+            {text}<br/>{subtext}
           </Card.Body>
-          
-          <Card.Body>
+          {/* <Card.Body>
            {subtext}
-          </Card.Body>
+          </Card.Body> */}
         </Card>
+
+
+        <br />
         {/* <Card className="bg-light" style={{ width: "30rem" , textAlign: "center"}}>
           <Card.Body>
             <Card.Title>{text}</Card.Title>
@@ -21,7 +33,6 @@ export default function PageHeader({text, subtext}){
             </Card.Subtitle>
           </Card.Body>
         </Card> */}
-        <br />
       </>
     );
 }
