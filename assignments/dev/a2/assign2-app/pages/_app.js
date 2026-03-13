b950f269ff92bd3e5aa9ca37a6c531b32b24a8e3
Niki9526 <n01591561@humber.ca>
2026-03-13 19:26:10 -0400

    added new bootstrap styles for the theme

--- assignments/dev/a2/assign2-app/pages/_app.js
+++ assignments/dev/a2/assign2-app/pages/_app.js
@@ -1,7 +1,8 @@
 import Layout from "@/components/Layout";
 import { SWRConfig } from 'swr';
 import "@/styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import '@/styles/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
   const fetcher = async (...args) => {
