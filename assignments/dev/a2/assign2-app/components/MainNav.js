19636134b805d272a48daaa9997d006afc519c87
Niki9526 <n01591561@humber.ca>
2026-03-13 19:26:44 -0400

    added a link in nav bar for favourites route

--- assignments/dev/a2/assign2-app/components/MainNav.js
+++ assignments/dev/a2/assign2-app/components/MainNav.js
@@ -10,6 +10,7 @@ export default function MainNav () {
             <Nav className="me-auto">
               {/* <Nav.Link as={Link} href="/">Books</Nav.Link> */}
               <Nav.Link as={Link} href="/about">About</Nav.Link>
+              <Nav.Link as={Link} href="/favourites">Favourites</Nav.Link>
             </Nav>
           </Container>
         </Navbar>
