d140435078a1e3db37214e6426a29394fbfccc3e
Niki9526 <n01591561@humber.ca>
2026-03-13 20:43:36 -0400

    deleted old card comments, removed old line break

--- assignments/dev/a2/assign2-app/components/PageHeader.js
+++ assignments/dev/a2/assign2-app/components/PageHeader.js
@@ -3,36 +3,17 @@ import {Card } from "react-bootstrap";
 export default function PageHeader({text, subtext}){
     return (
       <>
-        {/* <p>PageHeader</p> */}
-        {/* <div class="card border-light mb-3" style="max-width: 20rem;">
-          <div class="card-header">Header</div>
-          <div class="card-body">
-            <h4 class="card-title">Light card title</h4>
-            <p class="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </p>
-          </div>
-        </div> */}
-        <Card style={{border: "none", textAlign: "center"}} className="bg-light">
+         <Card style={{border: "none", textAlign: "center" }} className="bg-light">
           <Card.Body>
-            {text}<br/>{subtext}
+            {text}
+            {subtext}
           </Card.Body>
           {/* <Card.Body>
            {subtext}
           </Card.Body> */}
         </Card>
+        <br/>
 
-
-        <br />
-        {/* <Card className="bg-light" style={{ width: "30rem" , textAlign: "center"}}>
-          <Card.Body>
-            <Card.Title>{text}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              {subtext}
-            </Card.Subtitle>
-          </Card.Body>
-        </Card> */}
       </>
     );
 }
