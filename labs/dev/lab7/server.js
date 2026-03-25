e8c52aa752b02c60106d69d67bb25656a854f622
Niki9526 <n01591561@humber.ca>
2026-03-24 23:34:50 -0400

    added endpoints to view all books protected and add book protected , added comments

--- labs/dev/lab7/server.js
+++ labs/dev/lab7/server.js
@@ -38,7 +38,7 @@ let strategy = new JwtStrategy(JwtOptions, function (jwt_payload, next){
     }
 });
 
-// tell passport to use out stagty
+// tell passport to use out stragety
 passport.use(strategy);
 
 // add passport as application-level middleware
@@ -68,24 +68,61 @@ app.post("/api/login",(req,res)=>{
     });
 })
 
-// app.get("/api/books",(req,res)=>{
+app.get("/api/books", passport.authenticate('jwt', { session: false }),(req,res)=>{
+    console.log(req.user)
+    console.log(req.user.role + " " + req.user.userName )
+    console.log( typeof req.user.role)
 
-// })
+    const role = req.user.role;
+    let filteredBooks = [];
+
+    if (role=== "admin") {
+        filteredBooks = books;
+    } else if (role === "editor") {
+        filteredBooks = books.filter(book => 
+            book.viewPermission === "editor" || book.viewPermission === "public"
+        );
+    } else {
+        filteredBooks = books.filter(book =>
+            book.viewPermission ==="public"
+        );
+    }
+    console.log(filteredBooks);
+
+    
+
+    res.status(200).json(filteredBooks);
+
+});
+
+app.post("/api/books", passport.authenticate('jwt', { session: false }),(req,res)=>{
+    const role = req.user.role;
+    console.log(role)
+    console.log(req.body)
+    const newId = books.length > 0 ? Math.max (...books.map(book => book.id)) +1:1;
+    console.log(newId)
+    if(role === "admin" || role === "editor") {
+        ///only admin or editor can add new books
+
+        const newBook = {
+            id: newId,
+            title: req.body.title,
+            author: req.body.author,
+            year: req.body.year,
+            viewPermission: req.body.viewPermission
+        }
+        
+        books.push(newBook);
+        res.status(201).json({message: `User: ${req.user.userName} added a new book called ${req.body.title}`,newBook} )
+
+    } else {
+        res.status(403).send({message: `User ${req.user.userName}. You do not sufficient permissions to add books!`})
+    }
+   
 
 
 
-
-// app.get("/",(req,res)=>{
-//     res.send(books)
-// })
-
-
-
-
-
-
-
-
+})
 userService.connect().then(()=>{
     console.log(`App (Mongo Db connected) is listening on:  ${HTTP_PORT}`)
 }).catch (err=>{
