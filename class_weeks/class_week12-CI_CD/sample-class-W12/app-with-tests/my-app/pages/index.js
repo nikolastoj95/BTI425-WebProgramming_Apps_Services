284fd7823be37802c0f7fa8742936b44bac85006
Niki9526 <n01591561@humber.ca>
2026-03-31 12:20:39 -0400

    added workflow with lint error

--- class_weeks/class_week12-CI_CD/sample-class-W12/app-with-tests/my-app/pages/index.js
+++ class_weeks/class_week12-CI_CD/sample-class-W12/app-with-tests/my-app/pages/index.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const geistSans = Geist({
+const geistSans = Geist(
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
