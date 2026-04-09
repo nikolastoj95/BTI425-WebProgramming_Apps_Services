f982b74d340fa149f930f523fc66afaf6de2dda4
Niki9526 <n01591561@humber.ca>
2026-04-09 17:41:14 -0400

    added useEffect to change time in clock component

--- class_weeks/class_week03/my-appw03/components/Clock.js
+++ class_weeks/class_week03/my-appw03/components/Clock.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function Clock ({locale="en-CA"}) {
-    // const dateGetterAndSetter =  useState(new Date()); // pass in useState is the starter value
+    // const dateGetterAndSetter =   useState(new Date()); // pass in useState is the starter value
 
     // const dateGetter = dateGetterAndSetter[0];
     // const dateSetter = dateGetterAndSetter[1];
@@ -10,17 +10,35 @@ export default function Clock ({locale="en-CA"}) {
 
     const [date, setDate] = useState(null);
 
+    // add value of date once componnt is mounted
+
     useEffect(()=> { // only runs when the component is "mounted"
-        setDate(new Date());
-    })
+         setDate(new Date()); // if changed this will re run
+
+         let myInterval = setInterval(()=> {
+            setDate(new Date());
+         }, 1000) // 1000 ms after set new Date
+
+         return () => { // clean up step
+            clearInterval(myInterval)
+
+         }
 
 
-   // let date = new Date();
+
+
+     },[]);
+
+
+    // let date = new Date();
 
     return (
         <>
-            {date?.toLocaleTimeString(locale)}
-            
+        {/* TO DO: Render Clock with Locale: {locale}
+           {/*{date?.toLocaleTimeString(locale)} */}  
+        {date?.toLocaleTimeString(locale)}
+        {/*update time once every second */}
+
 
         </>
     )
