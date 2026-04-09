3f225ddca866d964be84363ab1f80c9403ffd462
Niki9526 <n01591561@humber.ca>
2026-04-09 17:40:29 -0400

    added comments to Counter component

--- class_weeks/class_week03/my-appw03/components/Counter.js
+++ class_weeks/class_week03/my-appw03/components/Counter.js
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 export default function Counter({start = 0}){ // {prop}
     const [count, setCount] = useState(start);
 
+
     useEffect(()=> {
         setInterval(()=> {
           //  setCount(count +1);
-          setCount(currentCount =>{
+          setCount(currentCount =>{ // we need an  up to date version of "count" in the state to calculate  the next "count"
             return currentCount +1;
-          })
+          });
             
         },1000);
     },[]);
 
     return(
+        // render counter
         <>
             Counter: {count};
 
