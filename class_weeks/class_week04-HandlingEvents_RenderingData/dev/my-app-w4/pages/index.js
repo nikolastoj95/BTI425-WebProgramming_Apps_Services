68fa47e6b48b7a5a69cd9518d29bcdecd95fd9cf
Niki9526 <n01591561@humber.ca>
2026-04-09 17:48:01 -0400

    showing on Index.js Home the made componets, and my static props function to fetch that one data to pass as prop to static props comp

--- class_weeks/class_week04-HandlingEvents_RenderingData/dev/my-app-w4/pages/index.js
+++ class_weeks/class_week04-HandlingEvents_RenderingData/dev/my-app-w4/pages/index.js
@@ -1,8 +1,24 @@
+import ClickCounter from "@/components/ClickCounter";
+import Post from "@/components/Post";
+import RenderPratice from "@/components/RenderPractice";
+import StaticPost from "@/components/StaticPost";
 
+export async function getStaticProps() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts/20");
+    const data = await (res.json());
+    return {props: {staticPost: data}} 
+}
 
-export default function Home() {
+export default function Home({staticPost}) {
   return (
     <>
+    <RenderPratice /> <br />
+    <hr />
+    {/*{staticPost.title}<br />*/}
+    <StaticPost post={staticPost}/>
+
+    <Post /><br />
+    <ClickCounter />
       
     </>
   );
