0e1b6e384d62d790ddaa89d414464d6bfd9d2770
Niki9526 <n01591561@humber.ca>
2026-04-09 17:42:35 -0400

    deleted old sample code, added my made componments

--- class_weeks/class_week03/my-appw03/pages/index.js
+++ class_weeks/class_week03/my-appw03/pages/index.js
@@ -1,48 +1,34 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
 import Hello from "@/components/Hello";
 import Clock from "@/components/Clock";
 import Counter from "@/components/Counter";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   let message = "Hello World!";
+  let imgUrl = "https://images.adsttc.com/media/images/6196/b960/9a95/7a76/4f1e/5b68/newsletter/newnham-campus-food-hall-taylor-smyth-architects-20.jpg?1637267827"
   return (
-    
-    // <div>
-    //     <p>Hello World</p>
-    //     <p>BTI425</p>
-    //     {/* only one parent element */}
-    // </div>
-    <>
-      <Counter /> <br />
+    <> {/* <></> -  JSX Fragement */}
+      <Counter  /> <br />
       <Counter start={5} /> {/* as number put {number} */}
-      <Clock />
-      <Hello message="Hello BTI 425" exam="!!!!!"/>
+      <Counter start={10} /> <br />
+      <Clock /><br />
+      <Clock locale="fr-CA" />
+      <Hello message="Hello BTI 425" exclamation="!!!!!" something=" From Seneca" />
+      <Hello message="Welcome to Next.js" />
       <p>{message}</p>
       <br />
       <hr />
       
       <p className= "red">BTI425</p>
+      {/* <img src={imgUrl} alt="seneca"  /> */}
+      <Clock locale="ko-KO" />
       
 
       {/* only one parent element */}
     </>
-    
-
-  )
-
-  
-}
+    // <div>
+    //     <p>Hello World</p>
+    //     <p>BTI425</p>
+    //     {/* only one parent element */}
+    // </div>
+  ); 
+};
