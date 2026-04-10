2b24014994c6d851ca4c838d10b499dd1b73b8d5
Niki9526 <n01591561@humber.ca>
2026-04-10 18:16:58 -0400

    added readtoken, router, logout function with removeToken added new drop down nav to show Logout, fav, when logged in else register

--- assignments/dev/a3/assign3-app/components/MainNav.js
+++ assignments/dev/a3/assign3-app/components/MainNav.js
@@ -1,17 +1,58 @@
+import { readToken, removeToken } from "@/lib/authenticate";
 import Link from "next/link";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import { useRouter } from "next/router";
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 export default function MainNav () {
+     const token = readToken();
+     
+     const router = useRouter();
+
+    function logout () {
+      console.log('logging out!')
+      removeToken();
+      router.push('/login');
+      
+    }
+
     return (
       <>
-        <Navbar className="fixed-top navbar-dark bg-dark"  >
+        <Navbar className="fixed-top navbar-dark bg-dark">
           <Container>
-            <Navbar.Brand as={Link} href="/" >Nikola Stojanovic</Navbar.Brand>
+            <Navbar.Brand as={Link} href="/">
+              Nikola Stojanovic
+            </Navbar.Brand>
             <Nav className="me-auto">
               {/* <Nav.Link as={Link} href="/">Books</Nav.Link> */}
-              <Nav.Link as={Link} href="/about">About</Nav.Link>
-              <Nav.Link as={Link} href="/favourites">Favourites</Nav.Link>
+              <Nav.Link as={Link} href="/about">
+                About
+              </Nav.Link>
+              {/* <Nav.Link as={Link} href="/favourites">
+                Favourites
+              </Nav.Link> */}
             </Nav>
+            {token && (
+              <Nav>
+                <NavDropdown title={token.userName} id="basic-nav-dropdown">
+                  <NavDropdown.Item as={Link} href="/favourites" >Favourites</NavDropdown.Item>
+                  {/* <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item> */}
+                  {/* <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item> */}
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item  onClick={logout}>
+                    LogOut
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            )}
+            {!token && (
+              <Nav>
+                <Nav.Link as={Link} href="/register">Register</Nav.Link>
+              </Nav>
+            )}
           </Container>
         </Navbar>
         <br />
