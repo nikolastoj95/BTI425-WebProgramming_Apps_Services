cb169fa035f3ade8f031f2c582bc54c74e7782d9
Niki9526 <n01591561@humber.ca>
2026-04-10 16:05:00 -0400

    logining page where authentic user action takes place

---
+++ assignments/dev/a3/assign3-app/pages/login.js
@@ -0,0 +1,84 @@
+import { authenticateUser } from "@/lib/authenticate";
+import { getFavourites } from "@/lib/userData";
+import { favouritesAtom } from "@/store";
+import { useAtom } from "jotai";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { Alert, Button, Card, Col, Form, Row } from "react-bootstrap";
+
+export default function Login(){
+
+    const [favouritesList, setFavouritesList] = useAtom(favouritesAtom);
+
+    const router = useRouter();
+
+    const [user, setUser] = useState('');
+    const [password, setPassword] = useState('');
+    const [message,setMessage] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            await authenticateUser(user,password);
+            console.log("logged in");
+            //redirect to vechile to veciles route
+
+            await updateAtom();
+            router.push(`/`)
+            
+        } catch (err) {
+            console.log(err.message)
+            setMessage(err.message)
+        }
+       
+        console.log(`TODO: Submit Form userName is ${user} and password is ${password} `)
+    };
+
+    async function updateAtom () {
+        setFavouritesList(await getFavourites());   
+    }
+    
+    return (
+      <>
+        <Card bg="light">
+          <Card.Body>
+            <h2>Login</h2>Enter your login information below:
+          </Card.Body>
+        </Card>
+        <br />
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>User:</Form.Label>
+            <Form.Control
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+              type="text"
+              id="userName"
+              name="userName"
+            />
+          </Form.Group>
+          <br />
+          <Form.Group>
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              id="password"
+              name="password"
+            />
+          </Form.Group>
+          <br />
+          <Button variant="primary" className="pull-right" type="submit">
+            Login
+          </Button>
+        </Form>
+        {message && (
+          <>
+            <br />
+            <Alert variant="danger">{message}</Alert>
+          </>
+        )}
+      </>
+    );
+}
